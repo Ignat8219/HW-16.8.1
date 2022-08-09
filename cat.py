afaaec8086f90a20914be8f90d71319e517fecac
Ignat8219 <107505649+Ignat8219@users.noreply.github.com>
2022-08-09 18:37:44 +0300

    Update cat.py

--- cat.py
+++ cat.py
@@ -6,11 +6,3 @@ class Cat :
 
     def print_cat(self):
         print(f"Имя: {self.name}, Пол: {self.sex}, Возраст: {self.age}")
-# class Cat:
-#     def __init__(self, name, age, gender):
-#         self.name = name
-#         self.age = age
-#         self.gender = gender
-#
-#     def cat_info(self):
-#         print(f"Имя: {self.name} Возраст: {self.age} Пол: {self.gender}")
