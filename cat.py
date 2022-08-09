414eb08786ecb291bd4dfc97554cfed15578fa0e
Ignat8219 <107505649+Ignat8219@users.noreply.github.com>
2022-08-09 18:32:37 +0300

    Add files via upload

---
+++ cat.py
@@ -0,0 +1,16 @@
+class Cat :
+    def __init__(self, name, sex, age):
+        self.name = name
+        self.sex = sex
+        self.age = age
+
+    def print_cat(self):
+        print(f"Имя: {self.name}, Пол: {self.sex}, Возраст: {self.age}")
+# class Cat:
+#     def __init__(self, name, age, gender):
+#         self.name = name
+#         self.age = age
+#         self.gender = gender
+#
+#     def cat_info(self):
+#         print(f"Имя: {self.name} Возраст: {self.age} Пол: {self.gender}")
