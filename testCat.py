5c5f1adbb3e33daaa7d3ba7d637e24f787f2b40f
Ignat8219 <107505649+Ignat8219@users.noreply.github.com>
2022-08-09 18:38:07 +0300

    Update testCat.py

--- testCat.py
+++ testCat.py
@@ -4,13 +4,3 @@ sem = Cat("Сэм", "мальчик", 2)
 tom = Cat('Tom', "мальчик", 5)
 cats = (sem.print_cat(), tom.print_cat())
 print(cats)
-
-
-# from cat import Cat
-#
-# tom = Cat("Том",6,"мальчик")
-# grfld = Cat("Гарфилд",4,"мальчик")
-# lilit = Cat("Лилит",999,"девочка")
-#
-# cats = (tom.cat_info(), grfld.cat_info(), lilit.cat_info())
-# print(cats)
